24b0e4d413ea5b2ec2cf3b880189562557463422
Joe Araújo <42116724+Joe02@users.noreply.github.com>
2019-02-21 11:25:11 -0800

    Coordinates of a Point

---
+++ 1041.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%f %f",&a,&b);
+    if(a>0&&b>0){
+        printf("Q1\n");
+    }
+    if(a<0&&b>0){
+        printf("Q2\n");
+    }
+    if(a>0&&b<0){
+        printf("Q4\n");
+    }
+    if(a<0&&b<0){
+        printf("Q3\n");
+    }
+    if(a==0&&b!=0){
+        printf("Eixo Y\n");
+    }
+    if(a!=0&&b==0){
+        printf("Eixo X\n");
+    }
+    if(a==0&&b==0){
+        printf("Origem\n");
+    }
+    return 0;
+}
